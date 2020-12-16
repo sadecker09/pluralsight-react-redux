7435d75d6d75b1933ea1fc3c6bb585e1f8ac031b
Sara Decker <sadecker09@gmail.com>
2020-12-16 12:16:43 -0700

    basic app layout and structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-//import { BrowserRouter as Router } from "react-router-dom";
-//import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-function HelloWorld() {
-  return <h1>Hello World</h1>;
-}
-
-render(<HelloWorld />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
