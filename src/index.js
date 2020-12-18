18a97628e32bcbbb6fda7e5c1fb71871dec5742b
Sara Decker <sadecker09@gmail.com>
2020-12-17 20:20:39 -0700

    (Redux config now completed) - instantiate store and provider at app's entry point

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore();
+// it can be useful to pass initial state into the store here if you are server rendering
+// or initializing Redux store w/ data from localStorage
+// this will override the default state declared in the reducer
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  // Provider connects React components to the store
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("root")
 );
