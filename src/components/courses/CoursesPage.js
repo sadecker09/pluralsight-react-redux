71460f5baf4fb2c0b43e490a7a7f9f490a87c572
Sara Decker <sadecker09@gmail.com>
2020-12-18 21:05:56 -0700

    start refactoring CoursesPage - since this is connected to store and Redux, needs to be container component; will move markup elsewhere

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,40 +5,14 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
-  // class field
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  // class field
-  // arrow functions inherit the binding context of their enclosing scope
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course: course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault(); // prevent postback
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        ></input>
-        <input type="submit" value="Save" />
         {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
