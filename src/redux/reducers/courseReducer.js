779f668aa74b85ca2a929b4e33b1e04f1c5dc8b9
Sara Decker <sadecker09@gmail.com>
2021-01-03 15:43:47 -0700

    centralize initial state of redux store in one file

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,13 +1,14 @@
 import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   // initialize state to empty array
   switch (action.type) {
     case types.CREATE_COURSE:
       // do not push directly onto state; return updated copy of state
       return [...state, { ...action.course }];
     case types.LOAD_COURSES_SUCCESS:
-        return action.courses;
+      return action.courses;
     default:
       // if reducer receive action it doesn't care about, return unchanged state
       return state;
---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,6 @@
+// declare initial state in a single file to document the Redux's store initial state
+// contrast with declaring initial state in each reducer file
+export default {
+  authors: [],
+  courses: [],
+};
