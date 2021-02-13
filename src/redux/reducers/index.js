0df7e0399263737b2ce376c3b0177d05cea78de1
Sara Decker <sdecker@west-inc.com>
2021-02-13 11:11:05 -0700

    documentation

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,6 +4,7 @@ import authors from "./authorReducer";
 import apiCallsInProgress from "./apiStatusReducer";
 
 const rootReducer = combineReducers({
+  // the key names supplied here will be the key names of the store's state object
   courses,
   authors,
   apiCallsInProgress,
